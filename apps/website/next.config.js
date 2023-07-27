fd1f8e225102c80311aaaa93409e27862a312185
nnecec <nnecec@outlook.com>
2023-07-28 00:15:37 +0800

    docs: init nextra

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+})
+
+module.exports = withNextra({
   reactStrictMode: true,
-  transpilePackages: ["ui"],
-};
+  transpilePackages: ['ui'],
+})
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
---
+++ apps/website/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>ZH</span>,
+  project: {
+    link: 'https://github.com/nnecec/zh',
+  },
+  // ...
+}
