14998a0e73d963f4afe11d9dbb04b79fc9a82af0
nnecec <nnecec@outlook.com>
2023-08-01 11:24:58 +0800

    docs: update

--- apps/website/theme.config.tsx
+++ apps/website/theme.config.tsx
@@ -6,7 +6,31 @@ import type { DocsThemeConfig } from 'nextra-theme-docs'
 import { Logo } from './components/logo'
 
 const config: DocsThemeConfig = {
+  chat: {
+    icon: (
+      <svg height={24} viewBox="0 0 24 24" width={24}>
+        <g>
+          <path
+            d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"
+            fill="currentColor"
+          ></path>
+        </g>
+      </svg>
+    ),
+    link: 'https://x.com/nnecec_cn',
+  },
+  docsRepositoryBase: 'https://github.com/nnecec/zh',
   editLink: {
+    component: ({ children, filePath }) => (
+      <a
+        className="nx-text-xs nx-font-medium nx-text-gray-500 hover:nx-text-gray-900 dark:nx-text-gray-400 dark:hover:nx-text-gray-100 contrast-more:nx-text-gray-800 contrast-more:dark:nx-text-gray-50"
+        href={`https://github.com/nnecec/zh/tree/main/apps/website/${filePath}`}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        {children}
+      </a>
+    ),
     text: 'Edit this page on GitHub →',
   },
   feedback: {
@@ -49,6 +73,10 @@ const config: DocsThemeConfig = {
     )
   },
   logo: <Logo size={36} />,
+  primaryHue: {
+    dark: 350,
+    light: 340,
+  },
   project: {
     link: 'https://github.com/nnecec/zh',
   },
